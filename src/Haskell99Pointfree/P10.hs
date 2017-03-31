8474ec2927d405620872334d3a9335b12f86a2c3
Sven Wille <wille.sven.public@gmail.com>
2017-03-31 22:05:33 +0200

    added stuff

--- src/Haskell99Pointfree/P10.hs
+++ src/Haskell99Pointfree/P10.hs
@@ -1,3 +1,15 @@
 module Haskell99Pointfree.P10
     (
     ) where
+
+import Control.Applicative
+import Data.List
+import Data.Bool.HT
+
+p10, p10' :: Eq a =>  [a] -> [(a,Integer)]
+
+
+p10 = map (liftA2 (,) head genericLength) .  group
+
+
+p10' = undefined -- ifThenElse
