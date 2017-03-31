1037f1161d2bf8de699f3e429b38da084110c7f1
Sven Wille <wille.sven.public@gmail.com>
2017-04-01 01:05:28 +0200

    added new p1 test

--- test/Tests/P1Tests.hs
+++ test/Tests/P1Tests.hs
@@ -8,6 +8,7 @@ import Test.QuickCheck
 import Haskell99Pointfree
 import Data.Maybe
 import Control.Exception
+import Data.Either
 
 p1Tests :: SpecWith ()
 p1Tests = describe "Problem 1: return the last element of a list" $ do
@@ -37,3 +38,12 @@ p1Tests = describe "Problem 1: return the last element of a list" $ do
   describe "testing properties of p1\'\'" $
     it "p1\'' with nonempty list" $ property
       (\(x::String) -> not  (null x) ==> (p1'' x == last x))
+
+  describe "testing version p1'''" $ do
+    it "p1''' with [1,2,3,4]" $
+      p1''' [1,2,3,4] `shouldBe` Right 4
+    it "p1''' with []" $
+      p1''' ([] :: [Int]) `shouldBe` Left "no last element for empty lists"
+  describe "testing properties of p1'''" $
+    it "p1''' with arbitrary lists" $ property
+      (\(x::String) -> (null x && isLeft (p1''' [])  ) ||   (p1''' x == Right  (last x)))
