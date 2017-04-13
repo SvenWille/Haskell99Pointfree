46a1dbcf8a4d3e9724b5564e60ca998fcce1d5b5
Sven Wille <wille.sven.public@gmail.com>
2017-04-13 03:16:20 +0200

    empty message

--- src/Haskell99Pointfree/P23.hs
+++ src/Haskell99Pointfree/P23.hs
@@ -1,20 +1,25 @@
 {-# LANGUAGE TupleSections #-}
 module Haskell99Pointfree.P23
-    (p23'
+    (p23, p23'
     ) where
 
 import System.Random
 import Control.Applicative
 import System.IO.Unsafe
 import Control.Monad
+import Control.Arrow
+import Data.List
 
---p23:: [a] -> Int -> IO [a]
 
---simplified version of p23 using unsafePerformIO (only useful for learning purposes)
+p23 :: [a] -> Int -> IO [Int]
+p23 =    ap (( . ( ( . take ) .  ( . flip fmap ))) . (.) . (<$>)  . snd . foldl ((( (fst . fst)  &&&   ( uncurry (flip(.)) . uncurry( ((!!):: [a] -> Int -> a) ***  flip (:) ))) . ) . (,):: (([a],[a]) -> Int -> (([a],[a]),Int)))
+          . (,[])::([a]-> ([a],[a])) ) ( ( `liftM` newStdGen) . randomRs . (0,) . subtract 1 . length)
+
+--alternative version using unsafePerformIO (only useful for learning purposes, since unsafePerformIO doesn't reevaluate getStdGen which results in getting the same )
 p23' :: [a] -> Int ->  [a]
-p23' =   ((reverse . snd ) . )   .  ap (  (.) . foldl  (uncurry(  ap (   (  . ) . (.) . (,) )    (  (  .  flip (:) ) . flip (.)  .   (!!)  )  )  )   . (,[])   )   ( (   . take  ) . flip id .   flip randomRs (unsafePerformIO getStdGen) . (0,) . subtract 1 . length )
+p23' = ((reverse . snd ) . ) . ap (  (.) . foldl  (uncurry( ap ( ( . ) . (.) . (,) ) ( ( . flip (:) ) . flip (.) . (!!)  ))) . (,[])) ( ( . take  ) . flip id .   flip randomRs (unsafePerformIO newStdGen) . (0,) . subtract 1 . length )
 
-{-
 
-p23 :: []
--}
+-- version using
+p23'' :: [a] -> Int -> [a]
+p23'' = undefined
---
+++ src/Haskell99Pointfree/P95.hs
@@ -0,0 +1,13 @@
+module Haskell99Pointfree.P95
+    (
+    ) where
+
+--using a list comprehension
+{-
+p95 :: Int -> String
+p95 = []
+-}
+--using until
+
+
+--using foldl
