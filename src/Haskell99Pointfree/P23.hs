6b74c10a33f2546024028f8eb975d05595f14ca6
Sven Wille <wille.sven.public@gmail.com>
2017-04-13 13:21:58 +0200

    added solution to p23

--- src/Haskell99Pointfree/P23.hs
+++ src/Haskell99Pointfree/P23.hs
@@ -10,16 +10,26 @@ import Control.Monad
 import Control.Arrow
 import Data.List
 
+--version where parts of the function are split up for readability
+p23 :: [a] -> Int -> IO [a]
+p23 =   (fmap snd . )  .   ap (( . helper1 ) . (.) .  fmap . foldl  helper3 . (,[])) helper2
 
-p23 :: [a] -> Int -> IO [Int]
-p23 =    ap (( . ( ( . take ) .  ( . flip fmap ))) . (.) . (<$>)  . snd . foldl ((( (fst . fst)  &&&   ( uncurry (flip(.)) . uncurry( ((!!):: [a] -> Int -> a) ***  flip (:) ))) . ) . (,):: (([a],[a]) -> Int -> (([a],[a]),Int)))
-          . (,[])::([a]-> ([a],[a])) ) ( ( `liftM` newStdGen) . randomRs . (0,) . subtract 1 . length)
+helper1 :: IO [Int] -> Int -> IO [Int]
+helper1  =  ( . take ) .  flip fmap
 
---alternative version using unsafePerformIO (only useful for learning purposes, since unsafePerformIO doesn't reevaluate getStdGen which results in getting the same )
-p23' :: [a] -> Int ->  [a]
-p23' = ((reverse . snd ) . ) . ap (  (.) . foldl  (uncurry( ap ( ( . ) . (.) . (,) ) ( ( . flip (:) ) . flip (.) . (!!)  ))) . (,[])) ( ( . take  ) . flip id .   flip randomRs (unsafePerformIO newStdGen) . (0,) . subtract 1 . length )
+--first argument for "ap" in p23. Generates a list of indices, depending on the length of the input array
+helper2 :: [a] -> IO [Int]
+helper2 =  ( `liftM` newStdGen) . randomRs . (0,) . subtract 1 . length
+
+helper3 :: ([a],[a]) -> Int -> ([a],[a])
+helper3 = ( (&&&) (fst . fst)  (uncurry( uncurry (flip (.)) . (***) (!!)   (flip (:)))) . ) . (,)
 
 
 -- version using
-p23'' :: [a] -> Int -> [a]
-p23'' = undefined
+p23' :: [a] -> Int -> [a]
+p23' = undefined
+
+
+--alternative version using unsafePerformIO (only useful for learning purposes, since unsafePerformIO doesn't reevaluate getStdGen which results in getting the same )
+p23'' :: [a] -> Int ->  [a]
+p23'' = ((reverse . snd ) . ) . ap (  (.) . foldl  (uncurry( ap ( ( . ) . (.) . (,) ) ( ( . flip (:) ) . flip (.) . (!!)  ))) . (,[])) ( ( . take  ) . flip id .   flip randomRs (unsafePerformIO newStdGen) . (0,) . subtract 1 . length )
