0fcd36d8faed79acd74f8afc5792260adf4745d1
Sven Wille <wille.sven.public@gmail.com>
2017-04-14 21:19:43 +0200

    added a solution to p06

--- src/Haskell99Pointfree/P06.hs
+++ src/Haskell99Pointfree/P06.hs
@@ -6,23 +6,24 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.Extra
 import Data.List
-
+import Control.Arrow
+import Data.Bool.HT
 
 p06, p06', p06'', p06''', p06_4, p06_5 :: Eq a => [a] -> Bool
 
 
-p06 = null .  until(liftA2 (||) null (liftA2 (/=) head last)) (init . tail)
+p06 = ((<= 1) . length)  .  until(liftA2 (||) (  (<= 1) . length) (liftA2 (/=) head last)) (init . tail)
 
+--using mapAccumL
+p06' = and . snd . uncurry (mapAccumL  ( join ( ( . ( head . fst) ) .  (. (==)) . (.)  . (,) . snd)     . splitAt 1 )) .  second reverse . join (liftA3 if' ( odd . length) . (uncurry (flip((,) . tail ))  . ) ) (join(splitAt . flip div 2 . length))
 
-p06' = undefined  -- and .  mapAccumR
-
-
+--does redundant comparisons
 p06'' = and . liftA2 (zipWith (==)) id reverse
 
-
+--does redundant comparisons
 p06''' = ap (==) reverse
 
-
+--does unnecessary comparisons
 p06_4 = undefined -- join (foldl ( (==) . head ))
 
 
