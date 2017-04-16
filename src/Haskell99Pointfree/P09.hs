0b89c71ee686a50416167143435a1f6d9c046c1e
Sven Wille <wille.sven.public@gmail.com>
2017-04-16 22:13:33 +0200

    changed p09

--- src/Haskell99Pointfree/P09.hs
+++ src/Haskell99Pointfree/P09.hs
@@ -15,8 +15,8 @@ p09_1 :: Eq a => [a] -> [[a]]
 p09_1 = group
 
 
-p09_2 :: Eq a => [a] -> [[a]]
-p09_2 = join ( ( .  (((,[],[]) . head)  >>= (foldr  (join (   (   . ( ( . liftA2 (:) (^._2) (^._3) )  . liftA2 (,,) id (:[]))))))))   . (.) .  ( flip (liftA3 if') (liftA3 (,,) (^._1) (join( ( . (^._2))  . (:) .(^._1) )) (^._3) )    . (==) )        .  flip if' (const []) . null)
+p09_2 :: Eq a => [a] -> [[a]];
+p09_2 = join ( ( .  (((,[],[]) . head)  >>= (foldr  (join    (   . ( ( . liftA2 (:) (^._2) (^._3) )  . liftA2 (,,) id (:[]))))))   . (.) .  ( flip (liftA3 if') (liftA3 (,,) (^._1) (join( ( . (^._2))  . (:) .(^._1) )) (^._3) )    . ( . fst)  .((==):: Char -> Char -> Bool) ) )       .  flip if' (const []) . null)
 
 
 --using ap
