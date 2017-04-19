17035cede9cc2b7fc56a2676aa60f813cad7f49c
Sven Wille <wille.sven.public@gmail.com>
2017-04-19 02:04:20 +0200

    added a solution to p03

--- src/Haskell99Pointfree/P03.hs
+++ src/Haskell99Pointfree/P03.hs
@@ -7,6 +7,7 @@ import Data.Bool.HT (if')
 import Control.Monad
 import Control.Lens
 import Data.List (find) --used in p3_6
+import Control.Monad.Fix
 
 
 --insecure
@@ -41,17 +42,21 @@ p3_5 =  flip if' Nothing . ( . (<= 0))  .  (||) .  null
 p03_6 :: [a] -> Int -> Maybe a
 p03_6 =  ( fmap snd . )   .   ( .  ( (. fst) .  (==) ))  . flip find   . zip [1..]
 
-{-
+
 --using until
-p3_7 :: [a] -> Int -> Maybe a
-p3_7 =
--}
+p03_7 :: [a] -> Int -> Maybe a
+p03_7 =   join ( ( . alias1) . liftM2 ( `if'` Nothing) .  ( . (<= 0) ) . (||) . null )
+  where
+    alias1 = ( (  liftM2 ( `if'` Nothing)  null (Just . head)  . (^._3) . until  (liftM2 (||) (liftM2 (==) (^._1) (^._2)) (null . (^._3) ) )    (liftM3 (,,) ( (+1) . (^._1))  (^._2) (tail . (^._3))  )) . ) .  flip (1,,)
+
 --using uncurry
 
 --using ap
-
---using fix
-
+{-
+--using mfix
+p03_10 :: [a] -> Int -> Maybe a
+p03_10 =  flip (mfix (    . flip  () ))
+-}
 --using foldr
 p03_11 :: [a] -> Int -> Maybe a
 p03_11 = ( .  (flip foldr Nothing.  join .   ((flip .flip  if') (const . Just. snd)  (const id) . )  . ( . fst)  . (==)   ) ) . flip ($) . zip [1..]
