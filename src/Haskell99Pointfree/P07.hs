ad2a0f74a7ff6260609728e5d99034c77ac8314f
Sven Wille <wille.sven.public@gmail.com>
2017-04-21 19:27:23 +0200

    added a solution to p07

--- src/Haskell99Pointfree/P07.hs
+++ src/Haskell99Pointfree/P07.hs
@@ -1,22 +1,29 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable , TemplateHaskell #-}
 module Haskell99Pointfree.P07
     (
     ) where
 
 
-import Control.Lens.Prism
+import Control.Lens (makePrisms)
+import Control.Lens.Extras (is)
+import Control.Monad.Extra (ifM)
 
---using lebels
-data NestedList a = NestedList {children ::[NestedList a]} | Elem {val::a}
-{-
+--using Prisms
+data NestedList a = List {child ::[NestedList a]} | Elem {val::a}
+
+makePrisms ''NestedList
+
 --recursive
 p07_1 :: NestedList a -> [a]
-p07_1 = concatMap
+p07_1 =  ifM (is _Elem) ( (:[]) . val) (concatMap p07_1 . child)
 
---using prisms
+
+--using the derive library
 data NestedList2 a = NestedList2 [NestedList2 a] | Elem2 a
 
---using a stack (which is the build in list)
+
+{-
+--using a stack (which is the build-in list)
 p07_2 :: NestedList a -> [a]
 p07_2 =   until (null . fst ) () . (,[])
 -}
