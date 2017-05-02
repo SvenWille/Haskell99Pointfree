44abec4eff183926a1b04f10a78af290d03a5155
Sven Wille <wille.sven.public@gmail.com>
2017-05-02 08:44:16 +0200

    added solution p24_2

--- src/Haskell99Pointfree/P24.hs
+++ src/Haskell99Pointfree/P24.hs
@@ -1,10 +1,28 @@
+{-# LANGUAGE TupleSections #-}
 module  Haskell99Pointfree.P24
     (
     ) where
 
 
 import System.Random
+import Control.Lens
+import Control.Applicative
+import Control.Monad
+import Data.Bool.HT
+import Control.Monad
+import Data.List (nub)
+{-
+--complex solution
+p24_1 :: Int -> Int -> IO (Maybe [Int])
+p24_1 = liftA2 (flip if' (return Nothing)) . (>) <*>   (( Just . view _3 . flip fmap newStdGen) .) . ((unitl  ((== 0) . (^._1)) nextStep . ) .)   . (,,[],)
+  where
+    nextStep = ap (liftM3 ( . (,,,)) (^._1)  ) ( liftA2 randomR  ( (0,) . subtract 1 . length . view _2)  (view _4) ) . over _1 (subtract 1)
+-}
 
-
-p24_1 :: 
-p24_1 =
+--using nub with take
+p24_2 :: Int -> Int -> IO (Maybe [Int])
+p24_2 = liftM2 ( `if'` return Nothing) . (>) <*>   ( . (randomRs  . (1,))) . (flip fmap newStdGen .) . (.) . ( . nub) . (Just .) . take 
+{-
+--using until
+p24_3 :: Int -> Int -> IO (Maybe [Int])
+-}
