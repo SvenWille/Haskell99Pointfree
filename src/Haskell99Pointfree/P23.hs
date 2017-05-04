5680af28edc39b2a19338a333a7f84ab83b34a19
Sven Wille <SvenWille@users.noreply.github.com>
2017-05-04 09:05:56 +0200

    added solution p23_2

--- src/Haskell99Pointfree/P23.hs
+++ src/Haskell99Pointfree/P23.hs
@@ -9,8 +9,10 @@ import System.IO.Unsafe --only used in p23_2
 import Control.Monad
 import Control.Arrow
 import Data.List
+import Data.Bool.HT (if')
 
 --version where parts of the function are split up for readability
+--this version allows multiple draws
 p23_1 :: [a] -> Int -> IO [a]
 p23_1 =   (fmap snd . )  .   ap (( . helper1 ) . (.) .  fmap . foldl  helper3 . (,[])) helper2
   where
@@ -24,11 +26,13 @@ p23_1 =   (fmap snd . )  .   ap (( . helper1 ) . (.) .  fmap . foldl  helper3 . 
     helper3 :: ([a],[a]) -> Int -> ([a],[a])
     helper3 = ( (&&&) (fst . fst)  (uncurry( uncurry (flip (.)) . (***) (!!)   (flip (:)))) . ) . (,)
 
-{-
--- version using take and nub
-p23_2 :: [a] -> Int -> IO [a]
-p23_2  = ((( flip fmap  newStdGen . ) . ( . take) ) . )  .  ( . ((.) . (.)) )  . flip ( . )  .  flip ( . )  . (nub . ) .  randomRs . (0,) . subtract 1 . length <*>  map  . (!!)
--}
+
+-- version using take and nub, If the index is too large or negative , Nothing is returned
+p23_2 :: [a] -> Int -> IO (Maybe [a])
+p23_2  = liftA2 (`if'` return  Nothing) . liftA2 (||) (<0) . (<) . length <*> trueBranch
+  where
+    trueBranch = ((( flip fmap  newStdGen . ) . ( . (take:: Int -> [a] -> [a]))) . ) . ( . (((Just .) . ) . (.) )) . flip (.) . flip ( . ) . (nub . ) . randomRs . (0,) . subtract 1 . length <*>  map . (!!)
+
 
 --alternative version using unsafePerformIO (only useful for learning purposes, since unsafePerformIO doesn't reevaluate getStdGen which results in getting the same )
 p23_3 :: [a] -> Int ->  [a]
