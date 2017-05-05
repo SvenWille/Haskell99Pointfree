419918290e18b98f91e17407bbbeb8291ef2e4e6
Sven Wille <SvenWille@users.noreply.github.com>
2017-05-05 04:49:37 +0200

    empty

--- src/Haskell99Pointfree/P62.hs
+++ src/Haskell99Pointfree/P62.hs
@@ -10,10 +10,11 @@ import Control.Applicative (liftA2, liftA3 , (<*>))
 import Control.Monad.Extra (ifM)
 import Control.Lens (makePrisms)
 import Control.Lens.Extras (is)
+import Control.Monad (ap)
 
 data Tree a = Empty  | Branch {val :: a , lft :: Tree a, rgt :: Tree a} deriving (Show , Eq)
 
 makePrisms ''Tree
 --using fix
 p62_1 :: Tree a -> [a]
-p62_1 = fix ( ifM (is _Empty) (const []) . ifM (liftA2 (&&) (is _Empty . lft) (is _Empty. rgt)) (const []) .   (   liftA3  ((. (++)) . (.) . (:)) val  . ( . lft)  <*> ( . rgt)) )
+p62_1 = fix ( ifM (is _Empty) (const []) . ifM (liftA2 (&&) (is _Empty . lft) (is _Empty. rgt)) (const []) . ((ap ((:) . val) .) . liftA2 (++)  . ( . lft)  <*> ( . rgt)) )
